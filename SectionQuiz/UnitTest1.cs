7e2c34b77da45c7e1a7a2351f9d33b72c7d8a906
NSPreetham <50166305+NSPreetham@users.noreply.github.com>
2021-01-29 12:36:54 +1100

    Initial commit

---
+++ SectionQuiz/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SectionQuiz
+{
+    [TestClass]
+    public class SectionQ
+    {
+        [TestMethod]
+        public void Convert_F_To_C()
+        {
+            double C = (64 - 32) * .5556;
+            Console.WriteLine(C);
+            Assert.AreEqual(C, 17.779, 0.001);
+
+        }
+        [TestMethod]
+        public void Convert_C_To_F()
+        {
+            double F = (64 /.5556) + 32;
+            Console.WriteLine(F);
+            Assert.AreEqual(F, 147.190, 0.001);
+
+        }
+
+
+    }
+}
---
+++ Testing/UnitTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    [TestCategory("LoginFeature")]
+    public class UnitTest1
+    {
+        private int a;
+        [TestInitialize]
+        public void RunBeforeEveryTest()
+        {
+            a = 1;
+        }
+        
+        [TestMethod]
+        [TestCategory("Positive")]
+        [Description("these are demo unit tests")]
+        public void TestMethod1()
+        {
+          
+            var b = 2;
+            Assert.AreEqual(3, a + b);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+           
+            Assert.IsTrue(a == 1);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void TestMethod3()
+        {
+          Assert.AreNotEqual(1, a);
+        }
+    }
+}
